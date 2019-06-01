dfb4f9f4570dc8e02224f8f2ed5a36a73eaada1c
David Foote <david.a.foote@gmail.com>
2019-06-01 04:42:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
-def introduction(name, lanuguage)
+def introduction_with_language(name, lanuguage)
   puts "Hi, my name is #{name} and I am learning to program in #{lanuguage}."
 end
 
-introduction("David", "Ruby")
+introduction_with_language("David", "Ruby")
